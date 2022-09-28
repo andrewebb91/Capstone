d676f6e04ff830d25a8c9750493e547abc58486b
andrewebb91 <andrewebb91@gmail.com>
2022-09-27 19:35:55 -0500

    having issues with my Shopping cart view

--- components/views/Podcast.js
+++ components/views/Podcast.js
@@ -1,15 +1,20 @@
 import html from "html-literal";
-export default (state) => html``;
-<section>
-  <p>
-    A fun, innovative way of looking at today’s societal norms through
-    generational lenses. A tongue in cheek, no holds bar discussion between
-    various viewpoints that may agree or disagree, but often realize the answer
-    is somewhere in between. #theperspectivepodcast #podcast #whatdoyouthink
-    #watchuswork #youngprofessionals #everydaytalk #blackinmedia #dailygrind
-    #HBCU #UMES #XULA #FAMU #HOWARD #JSU #podcastersofinstagram #podcastlife
-    #blackpodcast #blkpodcastmatters #listen #supportuswesupportyou #dopepodcast
-    #marrythathoe #boogersandbooties
-  </p>
-  <div>//Insert image_2099//</div>
-</section>;
+export default (state) => html`
+  <section>
+    <p>
+      A fun, innovative way of looking at today’s societal norms through
+      generational lenses. A tongue in cheek, no holds bar discussion between
+      various viewpoints that may agree or disagree, but often realize the
+      answer is somewhere in between.
+    </p>
+
+    <p>
+      #theperspectivepodcast#theperspectivepodcast #podcast #whatdoyouthink
+      #watchuswork #youngprofessionals #everydaytalk #blackinmedia #dailygrind
+      #HBCU #UMES #XULA #FAMU #HOWARD #JSU #podcastersofinstagram #podcastlife
+      #blackpodcast #blkpodcastmatters #listen #supportuswesupportyou
+      #dopepodcast #marrythathoe #boogersandbooties
+    </p>
+    <div>//Insert image_2099//</div>
+  </section>
+`;
--- components/views/Shoppingcart.js
+++ components/views/Shoppingcart.js
@@ -1,2 +1,65 @@
 import html from "html-literal";
-export default (state) => html``;
+export default (state) => html` <>
+  <p id="SC">Shopping Cart</p>
+  <div>Place items here</div>
+  <p class="PI">Payment Information</p>
+  <form action="https://formspree.io/f/xnqrpqve" method="POST">
+    <label>
+      UserName:
+      <input type="name here" name="name here" />
+
+      <div>
+      Password
+      <input type="password" name="password" />
+      </div>
+    </label>
+
+
+    <!-- your other form fields go here -->
+    <button type="submit">Send</button>
+  </form>
+  <p class="SA">Shipping Address</p>
+  <form action="https://formspree.io/f/xnqrpqve" method="POST">
+    <label>
+      Full Name:
+      <input type="name here" name="name here" />
+      Country
+      <input type="country" name="country" />
+      Street Address
+      <input type="address" name="address"/>
+      Apt/Unit/Etc
+      <input type="unit" name="unit"/>
+      Zip Code
+      <input type="text" name="zip" />
+      City
+      <input type='city' name='city'/>
+      State
+      <select type='state' name='state'/>
+    </label>
+    <button type="submit">Send</button>
+  </form>
+  <div></div>
+  <p class="BA">Billing Address</p>
+  <form action="https://formspree.io/f/xnqrpqve" method="POST">
+      Full Name:
+      <input type="name here" name="name here" />
+      Country
+      <input type="country" name="country" />
+      Street Address
+      <input type="address" name="address"/>
+      Apt/Unit/Etc
+      <input type="unit" name="unit"/>
+      Zip Code
+      <input type="text" name="zip" />
+      City
+      <input type='city' name='city'/>
+      State
+      <select type='state' name='state'/>
+    </label>
+    <!-- your other form fields go here -->
+    <button type="submit">Thank you</button>
+    </form>
+
+
+
+</section>`;
