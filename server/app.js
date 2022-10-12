31ae45bb864b7b58d47030499873ee0430fab21a
andrewebb91 <andrewebb91@gmail.com>
2022-10-12 17:51:53 -0500

    slideshow animation needed

--- server/app.js
+++ server/app.js
@@ -16,11 +16,29 @@ db.once(
   "open",
   console.log.bind(console, "Successfully opened connection to Mongo!")
 );
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
 const logging = (request, response, next) => {
   console.log(`${request.method} ${request.url} ${Date.now()}`);
   next();
 };
 
+//middleware
+app.use(cors);
 app.use(express.json());
 app.use(logging);
 // Handle the request with HTTP GET method from http://localhost:4040/status
@@ -38,4 +56,4 @@ app
 const PORT = process.env.API_PORT || 4040;
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
-app.listen(4040, () => console.log("Listening on port 4040"));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
