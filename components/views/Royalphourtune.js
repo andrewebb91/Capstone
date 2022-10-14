e1c39b3e2803596ba038a1cff448d14698d149cd
andrewebb91 <andrewebb91@gmail.com>
2022-10-13 20:18:32 -0500

    table help

--- components/views/Royalphourtune.js
+++ components/views/Royalphourtune.js
@@ -13,5 +13,75 @@ export default (state) => html`
     <div>Logos</div>
     <img class="nice" src="${fancy}" alt="nice" />
     <img class="crown" src="${crown}" alt="crown" />
+
+    <div>
+      <section id="form">
+        <form action="https://formspree.io/f/xnqrpqve"  method="POST" action="">
+          <h3>Order Here</h3>
+          <div>
+            <label for="customer">Customer:</label>
+            <input
+              type="text"
+              name="customer"
+              id="customer"
+              placeholder="Name here"
+              required
+            />
+          </div>
+          </div>
+            <label for="size">Size:(type out the size)
+            <input
+              type="text"
+              name="size"
+              id="id"
+              placeholder="s, m, l, x-large"
+              required
+              />
+            <div>
+            </div>
+            <label for="design">Design:</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox1"
+          class="items1"
+          name="design"
+          value="custom"
+        />
+        <label for="top1">logo1</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox2"
+          class="items1"
+          name="design"
+          value="logo1"
+        />
+        <label for="top2">logo2</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox3"
+          class="items1"
+          name="design"
+          value="logo2"
+        />
+        <div></div>
+        <input type="submit" name="submit" value="Submit Order" />
+        </form>
+      </section>
+    </div>
+    <div></div>
+    <table id="pizzas">
+    <tr>
+      <th>Size</th>
+      <th>Design</th>
+    </tr>
+    ${state.tops
+      .map((tops) => {
+        return `<tr><td>${top.size}</td><td>${top.designjoin(" & ")}</td><td>${
+          tops.customer
+        }</td></tr>`;
+      })
+      .join("")}
+  </table>
+
   </section>
 `;
